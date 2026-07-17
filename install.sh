a194111fad786381beea3584045988ddbdf5f01a
Muhammad Enrico Djibril <djibrilenrico@gmail.com>
2026-07-17 17:11:27 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,9 +8,11 @@ cd bot_premium
 curl -s -L https://raw.githubusercontent.com/Enrico2146/RejoinRIC/refs/heads/main/bot_manager.sh -o bot_manager.sh
 curl -s -L https://raw.githubusercontent.com/Enrico2146/RejoinRIC/refs/heads/main/engine.sh -o engine.sh
 
-# Perintah ini lebih "brutal" untuk menghapus semua karakter yang aneh
-tr -d '\r' < bot_manager.sh > temp.sh && mv temp.sh bot_manager.sh
-tr -d '\r' < engine.sh > temp.sh && mv temp.sh engine.sh
+# Instal dos2unix dan gunakan untuk membersihkan file
+# -y digunakan agar tidak perlu konfirmasi manual saat install
+pkg install dos2unix -y > /dev/null 2>&1
+dos2unix bot_manager.sh > /dev/null 2>&1
+dos2unix engine.sh > /dev/null 2>&1
 
 # Beri izin eksekusi
 chmod +x bot_manager.sh engine.sh
